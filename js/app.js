3e4b7981004f7559444c07be7e5d3df381d0a70b
David Guillen <maiden8-8@hotmail.com>
2016-03-10 04:35:03 -0600

    Modifique la vista

--- js/app.js
+++ js/app.js
@@ -1,22 +1,70 @@
 angular.module("myApp", ['ngRoute'])
 
-	.config(['$routeProvider', function($routeProvider) {
+	.config(function($routeProvider) {
 	    $routeProvider
 	    	.when('/inicio', {
-	            templateUrl: 'html/inicio.html',
+	            templateUrl: 'html/inicio.html'
 	        })
-	        .when('/agregar', {
-	            templateUrl: 'html/agregar.html',
+	        .when('/registrar', {
+	            templateUrl: 'html/registrar.html'
 	        })
-	        .when('/detalle', {
+	        .when('/ingresar', {
+	            templateUrl: 'html/ingresar.html'
+	        })
+	        .when('/usuario/agregar', {
+	            templateUrl: 'html/agregar.html'
+	        })
+	        .when('/usuario/detalle', {
 	            templateUrl: 'html/detalle.html',
+	            controller: 'detalleCtrl'
 	        })
-	        .when('/registro', {
-	            templateUrl: 'html/registro.html',
+	        .when('/usuario/registro', {
+	            templateUrl: 'html/registro.html'
 	        })
-	        .when('/resumen', {
+	        .when('/usuario/resumen', {
 	            templateUrl: 'html/resumen.html',
+	            controller: 'resumenCtrl'
 	        })
 	        .otherwise({redirectTo: '/inicio'});
-	}])
+	})
+
+	.controller('registroCtrl',
+		['$scope', 
+			function ($scope){
+		
+			}
+		]
+	)
+
+	.controller('agregarCtrl',
+		['$scope', 
+			function ($scope){
+		
+			}
+		]
+	)
+
+	.controller('detalleCtrl',
+		['$scope',
+			function ($scope){
+				$scope.detalles = [
+				{transaccion: '213', fecha:'2323', detalle: '12312', monto:'3434'},
+				{transaccion: '213', fecha:'2323', detalle: '12312', monto:'3434'},
+				{transaccion: '213', fecha:'2323', detalle: '12312', monto:'3434'}
+				];
+			}
+		]
+	)
+
+	.controller('resumenCtrl',
+		['$scope', 
+			function ($scope){
+				$scope.resumenes = [
+				{fecha: '213', detalle:'2323', monto:'3434'},
+				{fecha: '213', detalle:'2323', monto:'3434'},
+				{fecha: '213', detalle:'2323', monto:'3434'}
+				];
+			}
+		]
+	)
 ;
